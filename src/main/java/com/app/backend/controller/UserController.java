546e89325da960f3b37bf9968fcda0a944bbff21
Matt McKeever <mmckeever23@gmail.com>
2023-01-10 23:14:02 -0600

    Account creation and user login working

--- src/main/java/com/app/backend/controller/UserController.java
+++ src/main/java/com/app/backend/controller/UserController.java
@@ -7,21 +7,17 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+//@RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     @Autowired
     private UserRepo repo;
 
-    @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody User userData) {
-        System.out.println(userData);
-        System.out.println(userData);
-        User user=repo.findByUserId(userData.getUserId());
-        if(user.getPassword().equals(userData.getPassword()))
-            return ResponseEntity.ok(user);
-        return (ResponseEntity<?>) ResponseEntity.internalServerError();
-    }
+    @PostMapping("/user")
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
 
+        System.out.println("Controller called");
+        return ResponseEntity.ok(repo.save(user));
+    }
 }
